7a4adbf516fe469cff6f2fbe5f8950f462a85195
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2020-05-06 11:32:32 +0200

    Add swip Change screen orientation

---
+++ GoldProjectTeam2/Assets/Scripts/Swipe.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+
+    public float minimalSwipeDistance = 150;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.touches[0];
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    endPosition = touch.position;
+                    AnalyzeSwipeDistance(startPosition, endPosition);
+                    break;
+            }
+        }
+    }
+
+    private void AnalyzeSwipeDistance(Vector2 start, Vector2 end)
+    {
+        // Distance
+        if (Vector2.Distance(start, end) > minimalSwipeDistance)
+        {
+            Debug.Log("swipe");
+        }
+
+    }
+}
