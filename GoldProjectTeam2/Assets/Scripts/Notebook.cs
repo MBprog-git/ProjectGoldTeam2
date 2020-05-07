929fd38dcd71c4d5fe8f5576844da03597bb02fc
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2020-05-07 11:39:05 +0200

    Add turn page

---
+++ GoldProjectTeam2/Assets/Scripts/Notebook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notebook : MonoBehaviour
+{
+    private GameObject turnPage;
+    private GameObject actualPage;
+
+    public GameObject buttonNextPage;
+    public GameObject buttonPreviousPage;
+
+    public void NextPage()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (gameObject.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                actualPage = gameObject.transform.GetChild(i).gameObject;
+                turnPage = gameObject.transform.GetChild(i+1).gameObject;
+                
+                actualPage.SetActive(false);
+                turnPage.SetActive(true);
+
+                if(i+1 >= 4)
+                {
+                    buttonNextPage.SetActive(false);
+                }
+                buttonPreviousPage.SetActive(true);
+
+                return;
+            }
+        }
+    }
+
+    public void PreviousPage()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (gameObject.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                actualPage = gameObject.transform.GetChild(i).gameObject;
+                turnPage = gameObject.transform.GetChild(i-1).gameObject;
+
+                actualPage.SetActive(false);
+                turnPage.SetActive(true);
+
+                if(i-1 <= 0)
+                {
+                    buttonPreviousPage.SetActive(false);
+                }
+                buttonNextPage.SetActive(true);
+                return;
+            }
+        }
+    }
+}
--- GoldProjectTeam2/Assets/Scripts/Swipe.cs
+++ GoldProjectTeam2/Assets/Scripts/Swipe.cs
@@ -22,29 +22,29 @@ public class Swipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 1)
-        {
-            var touch = Input.touches[0];
-            if (!isOnNoteBook)
-            {
-                switch (touch.phase)
-                {
-                    case TouchPhase.Began:
-                        startPosition = touch.position;
-                        break;
-                    case TouchPhase.Ended:
-                        endPosition = touch.position;
-                        AnalyzeSwipeDistance(startPosition, endPosition);
-                        break;
-                }
-            }
-            else
-            {
-                Debug.Log(touch.position);
-                GameObject maskClone = Instantiate(mask, touch.position, transform.rotation);
-                maskClone.transform.position = touch.position;
-            }
-        }
+        //if (Input.touchCount == 1)
+        //{
+        //    var touch = Input.touches[0];
+        //    if (!isOnNoteBook)
+        //    {
+        //        switch (touch.phase)
+        //        {
+        //            case TouchPhase.Began:
+        //                startPosition = touch.position;
+        //                break;
+        //            case TouchPhase.Ended:
+        //                endPosition = touch.position;
+        //                AnalyzeSwipeDistance(startPosition, endPosition);
+        //                break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //Debug.Log(touch.position);
+        //        GameObject maskClone = Instantiate(mask, touch.position, transform.rotation);
+        //        maskClone.transform.position = touch.position;
+        //    }
+        //}
     }
 
     private void AnalyzeSwipeDistance(Vector2 start, Vector2 end)
