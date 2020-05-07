157eca7661f8ebe143be21d6bd6bad5545bbf654
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-05-07 10:04:51 +0200

    Ajout Boutons, Pop journal, gestion Gomme+dessin

---
+++ GoldProjectTeam2/Assets/Scripts/BoutonMB.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutonMB : MonoBehaviour
+{
+    public int ActionID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Action()
+    {
+        switch (ActionID)
+        {
+            case 1:
+                if (!GameManager.instance.Journal.GetComponent<JournalMB>().activated)
+                {
+                    GameManager.instance.Journal.GetComponent<JournalMB>().CallIn();
+                }
+                break;
+            
+            case 2:
+                GameManager.instance.Journal.GetComponent<JournalMB>().CallOut();
+                break; 
+            case 3:
+                GameManager.instance.Journal.GetComponent<JournalMB>().Cangomme = !GameManager.instance.Journal.GetComponent<JournalMB>().Cangomme;
+                break;
+        }
+    }
+}
