7a464b13fe6cce85378524a9316c50c25d23e47b
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-05-11 19:47:10 +0200

    avancement de la journée

--- GoldProjectTeam2/Assets/Scripts/JournalMB.cs
+++ GoldProjectTeam2/Assets/Scripts/JournalMB.cs
@@ -43,26 +43,29 @@ public class JournalMB : MonoBehaviour
         {
            
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D[] hits2D = Physics2D.RaycastAll(mousePos, Vector3.forward);
             mousePos.z = 0;
+            RaycastHit2D[] hits2D = Physics2D.RaycastAll(mousePos, Vector3.back);
+           
 
             foreach (RaycastHit2D hit2D in hits2D)
             {
                 if (hit2D.collider.tag == "Dessin" && mousePos != OldPos)
                 {
-                    GameObject go = Instantiate(GOMME, mousePos, transform.rotation);
+                   /* GameObject go = Instantiate(GOMME, mousePos, transform.rotation);
                     go.transform.SetParent(StockMask.transform);
                     OldPos = mousePos;
                     countMask++;
 
-                    if (countMask > 20)
+                   if (countMask > 20)
                     {
                     //EFFET
                     Debug.Log("EFFET DESSIN");
-                    DeleteMask();
-                    }/*
-                    EffaceDetect(mousePos, hit2D.collider.GetComponent<SpriteRenderer>());
-                    if (nbPixelTouch > 1000)
+                        GameManager.instance.TestFonct();
+                    //DeleteMask();
+                    }*/
+                   
+                   EffaceDetect(mousePos, hit2D.collider.gameObject.GetComponent<SpriteRenderer>());
+                    /*if (nbPixelTouch > 1000)
                     {
                         Debug.Log("EFFECT");
                         nbPixelTouch = 0;
@@ -168,28 +171,47 @@ public class JournalMB : MonoBehaviour
         }
     }
 
-    /* public void EffaceDetect(Vector2 mousePos, SpriteRenderer maskImage)
+     public void EffaceDetect(Vector2 mousePos, SpriteRenderer maskImage)
      {
-     Vector2 sprite_size = GetComponent<SpriteRenderer>().sprite.rect.size;
-         Debug.Log("Hoy");
+        Texture2D t = maskImage.sprite.texture;
+        //Vector2 sprite_size = GetComponent<SpriteRenderer>().sprite.rect.size;
      Vector2 min = new Vector2(mousePos.x - rayonCleaner, mousePos.y - rayonCleaner);
      Vector2 max = new Vector2(mousePos.x + rayonCleaner, mousePos.y + rayonCleaner);
+        
+        Debug.Log(mousePos);
+  
+       // Debug.Log(min+"/"+max);
 
                  for (int i = (int) min.x; i <= max.x; i++)
                  {
                      for (int j = (int) min.y; j <= max.y; j++)
                      {
-                         if (Vector2.Distance(new Vector2(i, j), mousePos) <= rayonCleaner)
+             
+                        // if (Vector2.Distance(new Vector2(i, j), mousePos) <= rayonCleaner)
                          {
-                             if (maskImage.sprite.texture.GetPixel(i, j).a != 0.0f)
+                    Debug.Log(t.GetPixel(i, j));
+                             if (t.GetPixel(i, j).a == 0.0f)
                              {
-                                 maskImage.sprite.texture.SetPixel(i, j, new Color(0,0,0,0));
-                                 nbPixelTouch += 1;
+                Debug.Log("Hoy");
+                                 t.SetPixel(i, j,  Color.clear);
+                        t.Apply();
+                                                nbPixelTouch += 1;
 
-                             }
+                       /* Texture2D t = maskImage.sprite.texture;
+                        Color32[] pix = t.GetPixels32();
+                        Debug.Log(pix.Length);*/
+                    }
 
-                         }
+                    }
                      }
                  }
-     }*/
+     }
+    private void OnDrawGizmos()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 min = new Vector2(mousePos.x - rayonCleaner, mousePos.y - rayonCleaner);
+        Vector2 max = new Vector2(mousePos.x + rayonCleaner, mousePos.y + rayonCleaner);
+        mousePos.z = 0;
+        Gizmos.DrawCube(mousePos,Vector3.one * rayonCleaner);
+    }
 }
