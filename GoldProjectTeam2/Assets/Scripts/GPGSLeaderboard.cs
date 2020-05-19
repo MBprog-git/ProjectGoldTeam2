10d2ed3e5a65382fd17034c28796c5070fb9131f
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2020-05-19 16:46:05 +0200

    proto leaderboard

---
+++ GoldProjectTeam2/Assets/Scripts/GPGSLeaderboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPGSLeaderboard : MonoBehaviour
+{
+    public void OpenLeaderboard()
+    {
+        Social.ShowLeaderboardUI();
+    }
+
+    public void UpdateLeaderboard()
+    {
+        if (PlayerPrefs.GetFloat("ScoreToUpdate", 0) == 0)
+        {
+            return;
+        }
+        Social.ReportScore(PlayerPrefs.GetInt("ScoreToUpdate", 1), GPGSIds.leaderboard_best_time, (bool success) =>
+       {
+           if (success)
+           {
+               PlayerPrefs.SetFloat("ScoreToUpdate", 0);
+           }
+       });
+    }
+}
---
+++ GoldProjectTeam2/Assets/Scripts/Score.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private float time;
+    private float minutes;
+    private float seconds;
+    private bool isTimerRunning = false;
+
+    private string timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTimerRunning)
+        {
+            time += Time.deltaTime;
+            minutes = Mathf.Floor(time / 60);
+            seconds = time % 60;
+
+            timer = string.Format("{0:00} : {1:00}", minutes, seconds);
+            Debug.Log(time);
+            Debug.Log(timer);
+        }
+    }
+
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isTimerRunning = false;
+        PlayerPrefs.SetFloat("TimeToUpdate", PlayerPrefs.GetFloat("TimeToUpdate",0) + time);
+        time = 0;
+    }
+}
