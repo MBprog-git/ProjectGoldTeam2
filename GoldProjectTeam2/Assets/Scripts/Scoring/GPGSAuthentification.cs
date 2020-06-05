f6c95798ac8f1f2cd18a1f46050e634dbec55953
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2020-06-05 19:54:44 +0200

    leaderboard test game v6

--- GoldProjectTeam2/Assets/Scripts/Scoring/GPGSAuthentification.cs
+++ GoldProjectTeam2/Assets/Scripts/Scoring/GPGSAuthentification.cs
@@ -7,53 +7,55 @@ using UnityEngine.SocialPlatforms;
 
 public class GPGSAuthentification : MonoBehaviour
 {
-    public static PlayGamesPlatform platform;
+    //public static PlayGamesPlatform platform;
 
     // Start is called before the first frame update
     void Awake()
     {
-        if (platform == null)
-        {
-            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
-            PlayGamesPlatform.InitializeInstance(config);
-            PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        //PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
 
-            platform = PlayGamesPlatform.Activate();
-        }
-
-
-        Social.Active.localUser.Authenticate(success => {
-            if (success)
-            {
-                Debug.Log("log in success");
-            }
-            else
-            {
-                Debug.Log("fail to log");
-            }
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) => {
+            // handle results
         });
+
+        //Social.Active.localUser.Authenticate(success =>
+        //{
+        //    if (success)
+        //    {
+        //        Debug.Log("log in success");
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("fail to log");
+        //    }
+        //});
     }
 
-    public void StartConnection()
-    {
-        if (platform == null)
-        {
-            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
-            PlayGamesPlatform.InitializeInstance(config);
-            PlayGamesPlatform.DebugLogEnabled = true;
+    //public void StartConnection()
+    //{
+    //    if (platform == null)
+    //    {
+    //        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+    //        PlayGamesPlatform.InitializeInstance(config);
+    //        PlayGamesPlatform.DebugLogEnabled = true;
 
-            platform = PlayGamesPlatform.Activate();
-        }
+    //        platform = PlayGamesPlatform.Activate();
+    //    }
 
-        Social.Active.localUser.Authenticate(success => {
-            if (success)
-            {
-                Debug.Log("log in success");
-            }
-            else
-            {
-                Debug.Log("fail to log");
-            }
-        });
-    }
+    //    Social.Active.localUser.Authenticate(success =>
+    //    {
+    //        if (success)
+    //        {
+    //            Debug.Log("log in success");
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("fail to log");
+    //        }
+    //    });
+    //}
 }
